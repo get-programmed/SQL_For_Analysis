b957d9d0668a917306498178ba5d1cd25f8fd8dd
Rahul singh <88223554+get-programmed@users.noreply.github.com>
2024-03-26 19:52:19 +0530

    Update SQL_Topic_Ex_2.sql
    Uploading more Examples

--- SQL_Topics/SQL_Topic_Ex_2.sql
+++ SQL_Topics/SQL_Topic_Ex_2.sql
@@ -193,7 +193,6 @@ WHERE customerid = ANY (
 	FROM rsc.onlinecustomers
 	WHERE customerid = 4)
 
-
 --- SQL ALL Operators
 SELECT * 
 FROM rsc.onlinecustomers 
@@ -248,4 +247,99 @@ SELECT
 		END AS 'Delivery Type'
 FROM rsc.orders
 
+--- SQL ISNULL() and COALESCE() Functions
+SELECT 
+	TOP 10 ISNULL( StoreID, 0) 
+	FROM rsc.dummy_sales_data
 
+SELECT 
+	TOP 10 COALESCE(StoreID,0) 
+	FROM rsc.dummy_sales_data
+
+--- SQL Stored Procedures for SQL Server
+--- EX 1
+CREATE PROCEDURE First_Function_Call
+AS
+SELECT TOP 50 * FROM rsc.dummy_sales_data;
+
+GO
+
+EXEC First_Function_call
+
+--- EX 2
+CREATE PROCEDURE First_Function_Call_2 @put_qty INT, @weather VARCHAR(30)
+AS
+SELECT 
+	TOP 50 * 
+	FROM rsc.dummy_sales_data
+	WHERE Quantity > @put_qty
+	AND Weather = @weather
+
+EXEC First_Function_Call_2 @put_qty = 3, @weather = 'Rainy'
+
+
+
+/* Operators
+
+SQL Arithmetic Operators 
++--------------------------+--------------------------------------------------------------+
+| Operator                 | Description                                                  |
++--------------------------+--------------------------------------------------------------+
+| +                        | Add                                                          |
+| -                        | Subtract                                                     |
+| *                        | Multiply                                                     |
+| /                        | Divide                                                       |
+| %                        | Modulo                                                       |
++--------------------------+--------------------------------------------------------------+
+
+SQL Bitwise Operators 
++--------------------------+--------------------------------------------------------------+
+| Operator                 | Description                                                  |
++--------------------------+--------------------------------------------------------------+
+| &                        | Bitwise AND                                                  |
+| |                        | Bitwise OR                                                   |
+| ^                        | Bitwise exclusive OR                                         |
++--------------------------+--------------------------------------------------------------+
+
+SQL Comparison Operators 
++--------------------------+--------------------------------------------------------------+
+| Operator                 | Description                                                  |
++--------------------------+--------------------------------------------------------------+
+| =                        | Equal to                                                     |
+| >                        | Greater than                                                 |
+| <                        | Less than                                                    |
+| >=                       | Greater than or equal to                                     |
+| <=                       | Less than or equal to                                        |
+| <>                       | Not equal to                                                 |
++--------------------------+--------------------------------------------------------------+
+
+SQL Compound Operators 
++--------------------------+--------------------------------------------------------------+
+| Operator                 | Description                                                  |
++--------------------------+--------------------------------------------------------------+
+| +=                       | Add equals                                                   |
+| -=                       | Subtract equals                                              |
+| *=                       | Multiply equals                                              |
+| /=                       | Divide equals                                                |
+| %=                       | Modulo equals                                                |
+| &=                       | Bitwise AND equals                                           |
+| ^-=                      | Bitwise exclusive equals                                     |
+| |*=                      | Bitwise OR equals                                            |
++--------------------------+--------------------------------------------------------------+
+
+SQL Logical Operators
++--------------------------+--------------------------------------------------------------+
+| Operator                 | Description                                                  |
++--------------------------+--------------------------------------------------------------+
+| ALL                      | TRUE if all of the subquery values meet the condition        |
+| AND                      | TRUE if all the conditions separated by AND is TRUE          |
+| ANY                      | TRUE if any of the subquery values meet the condition        |
+| BETWEEN                  | TRUE if the operand is within the range of comparisons       |
+| EXISTS                   | TRUE if the subquery returns one or more records             |
+| IN                       | TRUE if the operand is equal to one of a list of expressions |
+| LIKE                     | TRUE if the operand matches a pattern                        |
+| NOT                      | Displays a record if the condition(s) is NOT TRUE            |
+| OR                       | TRUE if any of the conditions separated by OR is TRUE        |
+| SOME                     | TRUE if any of the subquery values meet the condition        |
++--------------------------+--------------------------------------------------------------+
+*/
